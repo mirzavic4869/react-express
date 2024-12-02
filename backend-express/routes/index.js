f0243c06d3eeb6b28ac8acde799d5cb8db367a10
Mirzam Avicena <avicenamirzam@gmail.com>
2024-12-02 22:37:19 +0700

    make restful api login and get all users data

--- backend-express/routes/index.js
+++ backend-express/routes/index.js
@@ -4,14 +4,29 @@ const express = require('express');
 //init express router
 const router = express.Router();
 
+//import verifyToken
+const verifyToken = require('../middlewares/auth');
+
 //import register controller
 const registerController = require('../controllers/RegisterController');
 
-//import validate register
-const { validateRegister } = require('../utils/validators/auth');
+//import login controller
+const loginController = require('../controllers/LoginController');
+
+//import user controller
+const userController = require('../controllers/UserController');
+
+//import validate register and login
+const { validateRegister, validateLogin } = require('../utils/validators/auth');
 
 //define route for register
 router.post('/register', validateRegister, registerController.register);
+
+//define route for login
+router.post('/login', validateLogin, loginController.login);
+
+//define route for user
+router.get('/admin/users', verifyToken, userController.findUsers);
 
 //export router
 module.exports = router;
