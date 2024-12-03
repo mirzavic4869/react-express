31c36b7abe67d9160bfc21425d0399892412d2be
Mirzam Avicena <avicenamirzam@gmail.com>
2024-12-03 10:53:40 +0700

    make restful API insert data users

--- backend-express/routes/index.js
+++ backend-express/routes/index.js
@@ -19,6 +19,9 @@ const userController = require('../controllers/UserController');
 //import validate register and login
 const { validateRegister, validateLogin } = require('../utils/validators/auth');
 
+//import validate user
+const { validateUser } = require('../utils/validators/user');
+
 //define route for register
 router.post('/register', validateRegister, registerController.register);
 
@@ -27,6 +30,9 @@ router.post('/login', validateLogin, loginController.login);
 
 //define route for user
 router.get('/admin/users', verifyToken, userController.findUsers);
+
+//define route for user
+router.post('/admin/users', verifyToken, validateUser, userController.createUser);
 
 //export router
 module.exports = router;
